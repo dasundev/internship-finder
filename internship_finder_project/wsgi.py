563b92c51354b80e8ed675fad7d90d2d9b31a86d
Dasun Tharanga <hello@dasun.dev>
2024-02-13 16:06:48 +0530

    chore: Rename project name

--- internship_finder_project/wsgi.py
+++ internship_finder_project/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for internship_finder project.
+WSGI config for internship_finder_project project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'internship_finder.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'internship_finder_project.settings')
 
 application = get_wsgi_application()
