fa787c249c2fe700d7973ed387d2ccc6ab2c4d4c
Dasun Tharanga <hello@dasun.dev>
2024-02-13 15:00:00 +0530

    feat: Add database creds

--- internship_finder/settings.py
+++ internship_finder/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'internship_finder.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'internship_finder',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '3306'
     }
 }
 
