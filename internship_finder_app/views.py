41e85257adc90f2fe75fc3a616f4b3981d31c48f
Dasun Tharanga <hello@dasun.dev>
2024-02-13 23:27:36 +0530

    Update views.py

--- internship_finder_app/views.py
+++ internship_finder_app/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from .forms import LoginForm
 
-# Create your views here.
+
+def user_login(request):
+    section = {'title': 'Login'}
+
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            user = authenticate(email=email, password=password)
+            if user is not None:
+                login(request, user)
+        else:
+            form = LoginForm(request.POST)
+            return render(request, 'auth/login.html', {'section': section, 'form': form})
+
+    return render(request, 'auth/login.html', {'section': section})
