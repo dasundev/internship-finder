dca921e150433ca33f93cb8ad0001807482acdae
Dasun Tharanga <hello@dasun.dev>
2024-02-16 13:28:19 +0530

    Update forms.py

--- internship_finder_app/forms.py
+++ internship_finder_app/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
 
 class LoginForm(forms.Form):
@@ -10,3 +12,11 @@ class RegisterForm(forms.Form):
     username = forms.CharField(required=True)
     email = forms.EmailField(required=True)
     password = forms.CharField(required=True)
+
+    def clean_username(self):
+        username = self.cleaned_data['username']
+
+        if User.objects.filter(username=username).exists():
+            raise forms.ValidationError("This username already taken. please choose another.")
+
+        return username
